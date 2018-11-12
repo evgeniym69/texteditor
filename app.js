eba09895343607833d21f8ee129c83f9e07ff4a1
Evgeniy <evgeniym69@gmail.com>
2018-11-12 03:26:38 +0200

    Work on HttpRequest

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-/*function getMockText () {
-    return new Promise(function (resolve, reject){
-        resolve("A year ago I was in the audience at a gathering of designers in San Francisco. There were four designers on stage, and two of them worked for me. I was there to support them. The topic of design responsibility came up, possibly brought up by one of my designers, I honestly don’t remember the details. What I do remember is that at some point in the discussion I raised my hand and suggested, to this group of designers, that modern design problems were very complex. And we ought to need a license to solve them.");
-    })
-}
 
-getMockText().then(console.log);*/
-
-
-var bold = document.getElementsByClassName("format-action")[0],
+let bold = document.getElementsByClassName("format-action")[0],
     italic = document.getElementsByClassName("format-action")[1],
     underline = document.getElementsByClassName("format-action")[2],
-    text = document.getElementById("file"),
-    clr = document.getElementById('color'),
+    textField = document.getElementById("file"),
+    synonymBtn = document.getElementsByClassName("format-action")[3],
     txt;
 
-//Easy way
+
+//First way for bold/italic/underline
 bold.addEventListener('click', function(){
   document.execCommand('bold', false, null);
 });
@@ -39,21 +32,9 @@ document.addEventListener('keydown', function(){
 
 });
 
-$("#color").spectrum({
-    color: '#000000',
-    showPalette: true,
-    showInput: true,
-    showInitial: true,
-    showInput: true,
-    preferredFormat: "hex",
-    showButtons: false,
-    change: function(color) {
-      color = color.toHexString();
-      document.execCommand('foreColor', false, color);
-    }
-});
 //Another way
 //Added three classes in CSS - bold, italic, underline
+//BUT HAVE SOME PROBLEMS WITH REALIZATION. I will rezolve this issue later for myself
 
 /*var getSelectedText = function() {
     txt = '';
@@ -76,4 +57,68 @@ bold.addEventListener('click', function(){
   if (a != '') a.classList.add('bold');
 });*/
 
+//Colorpicker Jquery
+$("#color").spectrum({
+    color: '#000000',
+    showPalette: true,
+    showInput: true,
+    showInitial: true,
+    showInput: true,
+    preferredFormat: "hex",
+    showButtons: false,
+    change: function(color) {
+      color = color.toHexString();
+      document.execCommand('foreColor', false, color);
+    }
+});
+
 //Datamuse synonym
+
+
+function getSynonym(btn) {
+ /* get selected text */
+var str;
+var doc = textField.innerText;
+if (doc.getSelection) {
+  str = doc.getSelection().toString().trim();
+  } else if (doc.selection && doc.selection.createRange) {
+  var range = doc.selection.createRange();
+  str = range.text.toString().trim();
+  }
+
+if (str != '') {
+  /* if text selected, block the button */
+  btn.disabled = true;
+  /* do request */
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.datamuse.com/words?rel_syn='+escape(str), false);
+  xhr.send();
+  /* unblock button */
+  btn.disabled = false;
+  if (xhr.status == 200) {
+    var data = JSON.parse(xhr.responseText);
+    if (data.length) {
+      var popup = document.createElement('div');
+      document.body.appendChild(popup);
+      popup.id = 'popup-window';
+
+      var popup_html = '';
+      for (var i = 0; i < data.length; i++) {
+        popup_html += '<div><a href="javascript:" onclick="doReplaceText(this)">'+data[i].word+'</a></div>';
+      }
+      popup.innerHTML = popup_html;
+    }
+  }
+}
+}
+function doReplaceText(link) {
+/* insert chosen text */
+document.execCommand('insertText', false, link.innerHTML);
+var popup = document.getElementById("popup-window");
+/* remove popup */
+popup.parentNode.removeChild(popup);
+}
+
+synonymBtn.addEventListener('click', function(){
+  getSynonym(this);
+})
